9bc4ac8597269de62f4d671e66ae3b1759c69da1
jolaya182 <jolaya182@gmail.com>
2021-03-24 16:54:48 -0400

    update the files for the server. deleted server.js and added testServer.js

--- src/tests/jest/index.test.js
+++ src/tests/jest/index.test.js
@@ -14,7 +14,7 @@
 import 'regenerator-runtime/runtime';
 
 const supertest = require('supertest');
-const server = require('../../server/server');
+const server = require('../../server/testServer');
 const { outdoorsysequence, norvig } = require('../puppeteer/constants');
 
 const request = supertest(server);
